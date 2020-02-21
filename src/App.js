5653c9b4b8bee270310117269ec1758cf3ca86d9
ernesthenry <henry38ernest@gmail.com>
2020-02-21 22:10:30 +0300

    Use composition

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const people = [
+  {name: 'Tyler'},
+  {name: 'James'},
+  {name: 'Harris'},
+  {name: 'Ernest'}
+  ]
+
+class ContactList extends React.Component{
+  render()  {
+    return(
+      <ol>
+        {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+        ))}
+        </ol>
+    );
+  
+  }
+}
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="App">
+        <ContactList />
+
+      </div>
+    )
+   
+    
+
+  }
 }
 
 export default App;
+
